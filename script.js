bd8cdc2b83f65a6bb0e8743f437f1bfb0598825d
Mastermind <mastermindx33@gmail.com>
2023-04-10 18:57:34 +0530

    Fix JS Linters

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-const menu = document.getElementById("ham-menu");
-const ham = document.getElementById("menu");
+const menu = document.getElementById('ham-menu');
+const ham = document.getElementById('menu');
 
 function clickMenu() {
-  menu.style.display = "block";
-  ham.style.display = "none";
+  menu.style.display = 'block';
+  ham.style.display = 'none';
 }
 
 function clickX() {
-  menu.style.display = "none";
-  ham.style.display = "block";
+  menu.style.display = 'none';
+  ham.style.display = 'block';
 }
+
