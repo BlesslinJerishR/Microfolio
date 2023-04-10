f374cc336c1a68191a993b71517a52b4b0b055de
Mastermind <mastermindx33@gmail.com>
2023-04-10 18:51:04 +0530

    Add script.js

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 const menu = document.getElementById("ham-menu");
 const ham = document.getElementById("menu");
-let num = 0;
+
 function clickMenu() {
-  console.log("I am clicked");
   menu.style.display = "block";
   ham.style.display = "none";
 }
 
 function clickX() {
-  console.log("I am X");
   menu.style.display = "none";
   ham.style.display = "block";
 }
