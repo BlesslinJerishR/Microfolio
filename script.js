de800f704e9219a8a5b0b3ed7f1bbc4c33193149
Mastermind <mastermindx33@gmail.com>
2023-04-13 14:12:49 +0530

    Fix JS linter error

--- script.js
+++ script.js
@@ -35,15 +35,13 @@ for (const skill of skills) {
 li.classList.add('badge');
 
 const para = document.getElementById('js-p');
-const lorem =
-  "Lorem Ipsum is simply dummy text of the \nprinting and typesetting industry. Lorem \nIpsum has been the industry's standard \ndummy text ever since the 1500s, when \nan unknown printer took a galley of type \nand scrambled it 1960s.\n";
+const lorem = "Lorem Ipsum is simply dummy text of the \nprinting and typesetting industry. Lorem \nIpsum has been the industry's standard \ndummy text ever since the 1500s, when \nan unknown printer took a galley of type \nand scrambled it 1960s.\n";
 const p = document.createElement('p');
 p.innerText = lorem;
 para.appendChild(p);
 
 const para2 = document.getElementById('js-p2');
-const lorem2 =
-  "Lorem Ipsum is simply dummy text of the \nprinting and typesetting industry. Lorem \nIpsum has been the industry's standard \ndummy text ever since the 1500s, when \nan unknown printer took a galley of type \nand scrambled it 1960s.\n";
+const lorem2 = "Lorem Ipsum is simply dummy text of the \nprinting and typesetting industry. Lorem \nIpsum has been the industry's standard \ndummy text ever since the 1500s, when \nan unknown printer took a galley of type \nand scrambled it 1960s.\n";
 const p2 = document.createElement('p');
 p2.innerText = lorem2;
 para2.appendChild(p2);
