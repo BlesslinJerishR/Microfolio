165c8b8fb96bb7131d7eea0480bff83d30113e73
Mastermind <mastermindx33@gmail.com>
2023-04-13 18:33:15 +0530

    Fix eslint error

--- script.js
+++ script.js
@@ -24,24 +24,26 @@ const h1 = document.createElement('h1');
 h1.innerText = heading;
 he1.appendChild(h1);
 
-const ul = document.getElementById('badges');
-const skills = ['Ruby on Rails', 'CSS', 'JS'];
-const li = document.createElement('li');
-for (const skill of skills) {
-  const text = document.createTextNode(skill);
-  li.appendChild(text);
-  ul.appendChild(li);
-}
-li.classList.add('badge');
+// Skills Badge List  - For popup
+
+// const ul = document.getElementById('badges');
+// const skills = ['Ruby on Rails', 'CSS', 'JS'];
+// const li = document.createElement('li');
+// for (const skill of skills) {
+//   const text = document.createTextNode(skill);
+//   li.appendChild(text);
+//   ul.appendChild(li);
+// }
+// li.classList.add('badge');
 
 const para = document.getElementById('js-p');
-const lorem = "Lorem Ipsum is simply dummy text of the \nprinting and typesetting industry. Lorem \nIpsum has been the industry's standard \ndummy text ever since the 1500s, when \nan unknown printer took a galley of type \nand scrambled it 1960s.\n";
+const lorem = 'Lorem Ipsum is simply dummy text of the \nprinting and typesetting industry. Lorem \nIpsum has been the industry\'s standard \ndummy text ever since the 1500s, when \nan unknown printer took a galley of type \nand scrambled it 1960s.\n';
 const p = document.createElement('p');
 p.innerText = lorem;
 para.appendChild(p);
 
 const para2 = document.getElementById('js-p2');
-const lorem2 = "Lorem Ipsum is simply dummy text of the \nprinting and typesetting industry. Lorem \nIpsum has been the industry's standard \ndummy text ever since the 1500s, when \nan unknown printer took a galley of type \nand scrambled it 1960s.\n";
+const lorem2 = 'Lorem Ipsum is simply dummy text of the \nprinting and typesetting industry. Lorem \nIpsum has been the industry\'s standard \ndummy text ever since the 1500s, when \nan unknown printer took a galley of type \nand scrambled it 1960s.\n';
 const p2 = document.createElement('p');
 p2.innerText = lorem2;
 para2.appendChild(p2);
@@ -52,6 +54,7 @@ function pop() {
   pops.style.display = 'block';
 }
 pop('');
+
 function clickPopX() {
   pops.style.display = 'none';
 }
@@ -66,6 +69,7 @@ const form = qs('form');
 const name = qs('input[name="name"]');
 const email = qs('input[name="email"]');
 const txt = qs('textarea[name="text"]');
+const error = document.getElementById('error');
 
 form.addEventListener('submit', (event) => {
   const errorMessages = [];
@@ -84,4 +88,7 @@ form.addEventListener('submit', (event) => {
   } else {
     error.textContent = '';
   }
+  localStorage.setItem('name', name.value);
+  localStorage.setItem('email', email.value);
+  localStorage.setItem('text', txt.value);
 });
