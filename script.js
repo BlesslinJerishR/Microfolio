9b92ab4b9d5c80490c2bf635945353bff9525399
Mastermind <mastermindx33@gmail.com>
2023-04-13 17:59:35 +0530

    Fix eslint errors

--- script.js
+++ script.js
@@ -1,61 +1,59 @@
-const menu = document.getElementById("ham-menu");
-const ham = document.getElementById("menu");
+const menu = document.getElementById('ham-menu');
+const ham = document.getElementById('menu');
 
 function clickMenu() {
-  menu.style.display = "block";
-  ham.style.display = "none";
+  menu.style.display = 'block';
+  ham.style.display = 'none';
 }
-clickMenu("");
+clickMenu('');
 
 function clickX() {
-  menu.style.display = "none";
-  ham.style.display = "block";
+  menu.style.display = 'none';
+  ham.style.display = 'block';
 }
-clickX("");
+clickX('');
 
-const popup = document.getElementById("popup");
-let img = document.createElement("img");
-img.src = "/imgs/popup.png";
+const popup = document.getElementById('popup');
+const img = document.createElement('img');
+img.src = '/imgs/popup.png';
 popup.appendChild(img);
 
-let he1 = document.getElementById("popup-content");
-let heading = "Keeping track of \n hundreds of \n components";
-let h1 = document.createElement("h1");
+const he1 = document.getElementById('popup-content');
+const heading = 'Keeping track of \n hundreds of \n components';
+const h1 = document.createElement('h1');
 h1.innerText = heading;
 he1.appendChild(h1);
 
-var ul = document.getElementById("badges");
-let skills = ["Ruby on Rails", "CSS", "JS"];
-for (let skill of skills) {
-  var li = document.createElement("li");
-  var text = document.createTextNode(skill);
-  li.appendChild(text);
-  ul.appendChild(li);
-}
-li.classList.add("badge");
+const ul = document.getElementById('badges');
+const skills = ['Ruby on Rails', 'CSS', 'JS'];
+const li = document.createElement('li');
+// for (const skill of skills) {
+//   const text = document.createTextNode(skill);
+//   li.appendChild(text);
+//   ul.appendChild(li);
+// }
+li.classList.add('badge');
 
-let para = document.getElementById("js-p");
-let lorem =
-  "Lorem Ipsum is simply dummy text of the \nprinting and typesetting industry. Lorem \nIpsum has been the industry's standard \ndummy text ever since the 1500s, when \nan unknown printer took a galley of type \nand scrambled it 1960s.\n";
-let p = document.createElement("p");
+const para = document.getElementById('js-p');
+const lorem = 'Lorem Ipsum is simply dummy text of the \nprinting and typesetting industry. Lorem \nIpsum has been the industry\'s standard \ndummy text ever since the 1500s, when \nan unknown printer took a galley of type \nand scrambled it 1960s.\n';
+const p = document.createElement('p');
 p.innerText = lorem;
 para.appendChild(p);
 
-let para2 = document.getElementById("js-p2");
-let lorem2 =
-  "Lorem Ipsum is simply dummy text of the \nprinting and typesetting industry. Lorem \nIpsum has been the industry's standard \ndummy text ever since the 1500s, when \nan unknown printer took a galley of type \nand scrambled it 1960s.\n";
-let p2 = document.createElement("p");
+const para2 = document.getElementById('js-p2');
+const lorem2 = 'Lorem Ipsum is simply dummy text of the \nprinting and typesetting industry. Lorem \nIpsum has been the industry\'s standard \ndummy text ever since the 1500s, when \nan unknown printer took a galley of type \nand scrambled it 1960s.\n';
+const p2 = document.createElement('p');
 p2.innerText = lorem2;
 para2.appendChild(p2);
 
-let pops = document.getElementById("pop-outer");
+const pops = document.getElementById('pop-outer');
 
 function pop() {
-  pops.style.display = "block";
+  pops.style.display = 'block';
 }
 
 function clickPopX() {
-  pops.style.display = "none";
+  pops.style.display = 'none';
 }
 
 // Validate Contact Form
@@ -64,9 +62,10 @@ function qs(ClassorId) {
   return document.querySelector(ClassorId);
 }
 const form = qs('form');
-const name = qs('input[name="name"]');
-const email = qs('input[name="email"]');
-const txt = qs('textarea[name="text"]');
+const name = qs('input[name='name']');
+const email = qs('input[name='email']');
+const txt = qs('textarea[name='text']');
+const error = qs('error');
 
 form.addEventListener('submit', (event) => {
   const errorMessages = [];
