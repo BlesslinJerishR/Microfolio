0ebdaf1a22ac539e1e7f21b8a933e05fa8bddbd8
Mastermind <mastermindx33@gmail.com>
2023-04-13 13:48:54 +0530

    Fix JS linters

--- script.js
+++ script.js
@@ -14,19 +14,19 @@ function clickX() {
 clickX('');
 
 const popup = document.getElementById('popup');
-let img = document.createElement('img');
+const img = document.createElement('img');
 img.src = '/imgs/popup.png';
 popup.appendChild(img);
 
-let he1 = document.getElementById('popup-content');
-let heading = 'Keeping track of \n hundreds of \n components';
-let h1 = document.createElement('h1');
+const he1 = document.getElementById('popup-content');
+const heading = 'Keeping track of \n hundreds of \n components';
+const h1 = document.createElement('h1');
 h1.innerText = heading;
 he1.appendChild(h1);
 
 var ul = document.getElementById('badges');
-let skills = ['Ruby on Rails', 'CSS', 'JS'];
-for (let skill of skills) {
+const skills = ['Ruby on Rails', 'CSS', 'JS'];
+for (const skill of skills) {
   var li = document.createElement('li');
   var text = document.createTextNode(skill);
   li.appendChild(text);
@@ -34,21 +34,21 @@ for (let skill of skills) {
 }
 li.classList.add('badge');
 
-let para = document.getElementById('js-p');
-let lorem =
+const para = document.getElementById('js-p');
+const lorem =
   "Lorem Ipsum is simply dummy text of the \nprinting and typesetting industry. Lorem \nIpsum has been the industry's standard \ndummy text ever since the 1500s, when \nan unknown printer took a galley of type \nand scrambled it 1960s.\n";
-let p = document.createElement('p');
+const p = document.createElement('p');
 p.innerText = lorem;
 para.appendChild(p);
 
-let para2 = document.getElementById('js-p2');
-let lorem2 =
+const para2 = document.getElementById('js-p2');
+const lorem2 =
   "Lorem Ipsum is simply dummy text of the \nprinting and typesetting industry. Lorem \nIpsum has been the industry's standard \ndummy text ever since the 1500s, when \nan unknown printer took a galley of type \nand scrambled it 1960s.\n";
-let p2 = document.createElement('p');
+const p2 = document.createElement('p');
 p2.innerText = lorem2;
 para2.appendChild(p2);
 
-let pops = document.getElementById('pop-outer');
+const pops = document.getElementById('pop-outer');
 
 function pop() {
   pops.style.display = 'block';
