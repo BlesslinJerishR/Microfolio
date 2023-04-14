968feee4fe1c168d4d59cb03c51d6510946ad385
Mastermind <mastermindx33@gmail.com>
2023-04-14 12:52:12 +0530

    Add single quote in JS

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
-const menu = document.getElementById("ham-menu");
-const ham = document.getElementById("menu");
+const menu = document.getElementById('ham-menu');
+const ham = document.getElementById('menu');
 
 function clickMenu() {
-  menu.style.display = "block";
-  ham.style.display = "none";
+  menu.style.display = 'block';
+  ham.style.display = 'none';
 }
-clickMenu("");
+clickMenu('');
 
 function clickX() {
-  menu.style.display = "none";
-  ham.style.display = "block";
+  menu.style.display = 'none';
+  ham.style.display = 'block';
 }
-clickX("");
+clickX('');
 
-const popup = document.getElementById("popup");
-const img = document.createElement("img");
-img.src = "/imgs/popup.png";
+const popup = document.getElementById('popup');
+const img = document.createElement('img');
+img.src = '/imgs/popup.png';
 popup.appendChild(img);
 
-const he1 = document.getElementById("popup-content");
-const heading = "Keeping track of \n hundreds of \n components";
-const h1 = document.createElement("h1");
+const he1 = document.getElementById('popup-content');
+const heading = 'Keeping track of \n hundreds of \n components';
+const h1 = document.createElement('h1');
 h1.innerText = heading;
 he1.appendChild(h1);
 
@@ -36,61 +36,61 @@ he1.appendChild(h1);
 // }
 // li.classList.add('badge');
 
-const para = document.getElementById("js-p");
+const para = document.getElementById('js-p');
 const lorem =
-  "Lorem Ipsum is simply dummy text of the \nprinting and typesetting industry. Lorem \nIpsum has been the industry's standard \ndummy text ever since the 1500s, when \nan unknown printer took a galley of type \nand scrambled it 1960s.\n";
-const p = document.createElement("p");
+  'Lorem Ipsum is simply dummy text of the \nprinting and typesetting industry. Lorem \nIpsum has been the industry's standard \ndummy text ever since the 1500s, when \nan unknown printer took a galley of type \nand scrambled it 1960s.\n';
+const p = document.createElement('p');
 p.innerText = lorem;
 para.appendChild(p);
 
-const para2 = document.getElementById("js-p2");
+const para2 = document.getElementById('js-p2');
 const lorem2 =
-  "Lorem Ipsum is simply dummy text of the \nprinting and typesetting industry. Lorem \nIpsum has been the industry's standard \ndummy text ever since the 1500s, when \nan unknown printer took a galley of type \nand scrambled it 1960s.\n";
-const p2 = document.createElement("p");
+  'Lorem Ipsum is simply dummy text of the \nprinting and typesetting industry. Lorem \nIpsum has been the industry's standard \ndummy text ever since the 1500s, when \nan unknown printer took a galley of type \nand scrambled it 1960s.\n';
+const p2 = document.createElement('p');
 p2.innerText = lorem2;
 para2.appendChild(p2);
 
-const pops = document.getElementById("pop-outer");
+const pops = document.getElementById('pop-outer');
 
 function pop() {
-  pops.style.display = "block";
+  pops.style.display = 'block';
 }
-pop("");
+pop('');
 
 function clickPopX() {
-  pops.style.display = "none";
+  pops.style.display = 'none';
 }
-clickPopX("");
+clickPopX('');
 
 // Validate Contact Form
 
 function qs(ClassorId) {
   return document.querySelector(ClassorId);
 }
-const form = qs("form");
+const form = qs('form');
 const name = qs('input[name="name"]');
 const email = qs('input[name="email"]');
 const txt = qs('textarea[name="text"]');
-const error = document.getElementById("error");
+const error = document.getElementById('error');
 
-form.addEventListener("submit", (event) => {
+form.addEventListener('submit', (event) => {
   const errorMessages = [];
-  if (name.value.trim() === "") {
-    errorMessages.push("Name is required");
-  } else if (email.value.trim() === "") {
-    errorMessages.push("Email is required");
+  if (name.value.trim() === '') {
+    errorMessages.push('Name is required');
+  } else if (email.value.trim() === '') {
+    errorMessages.push('Email is required');
   } else if (email.value !== email.value.toLowerCase()) {
-    errorMessages.push("Email must be in lowercase");
-  } else if (txt.value.trim() === "") {
-    errorMessages.push("Message is required");
+    errorMessages.push('Email must be in lowercase');
+  } else if (txt.value.trim() === '') {
+    errorMessages.push('Message is required');
   }
   if (errorMessages.length > 0) {
     event.preventDefault();
-    error.textContent = errorMessages.join(".");
+    error.textContent = errorMessages.join('.');
   } else {
-    error.textContent = "";
+    error.textContent = '';
   }
-  localStorage.setItem("name", name.value);
-  localStorage.setItem("email", email.value);
-  localStorage.setItem("text", txt.value);
+  localStorage.setItem('name', name.value);
+  localStorage.setItem('email', email.value);
+  localStorage.setItem('text', txt.value);
 });
